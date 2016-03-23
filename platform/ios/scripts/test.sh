5a0f6ec72ad5056632aec346417fe6662aa0197b
John Firebaugh <john.firebaugh@gmail.com>
2016-03-23 15:10:09 -0700

    [ios] Run core unit tests in CI

--- platform/ios/scripts/test.sh
+++ platform/ios/scripts/test.sh
@@ -10,3 +10,12 @@ xcodebuild \
     -sdk iphonesimulator \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' \
     test
+
+xcodebuild \
+    -project ./build/ios-all/gyp/ios.xcodeproj \
+    -scheme 'test' \
+    -sdk iphonesimulator \
+    -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' \
+    build
+
+ios-sim launch `xcrun simctl get_app_container booted com.mapbox.MapboxGLUnitTest`
