49956d4e918aad91e0d21d996784e8e94274e45d
John Firebaugh <john.firebaugh@gmail.com>
2016-03-24 15:28:13 -0700

    [tests] Rationalize storage tests
    Properly divide Storage tests into DefaultFileSource, OnlineFileSource, and HTTPFileSource tests.

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,6 +1,7 @@
 #include <mbgl/test/util.hpp>
 
 GTEST_API_ int main(int argc, char *argv[]) {
+    auto server = std::make_unique<mbgl::test::Server>("test/storage/server.js");
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
